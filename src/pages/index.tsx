ad0921d14fbd1186f6e94b43d4b5863a392b0837
labaran1 <adlabaran@gmail.com>
2023-05-01 12:17:44 +0000

    about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Navbar from "../Components/Layouts/Navbar";
+import Styles from "../styles/Home.module.css";
 const Home = () => {
   return (
     <main>
       <Navbar />
+      <div className={`  ${Styles.about}`}>
+        <h1>Run, Publish & Deploy and code - anywhere</h1>
+      </div>
     </main>
   );
 };
