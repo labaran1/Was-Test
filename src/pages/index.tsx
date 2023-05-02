862462681b24c435d795b3eecaef7eaa6c6ee19b
labaran1 <adlabaran@gmail.com>
2023-05-02 22:59:47 +0000

    deploy section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,8 @@ import MonkeyIcon from "@/Components/Icons/MonkeyIcon";
 import BarIcon from "@/Components/Icons/BarIcon";
 import LoveIcon from "@/Components/Icons/LoveIcon";
 import ArrowDownIcon from "@/Components/Icons/ArrowDown";
+import WordPressIcon from "@/Components/Icons/WordPress";
+import DjangoIcon from "@/Components/Icons/Django";
 
 import { Button } from "@mui/material";
 const Home = () => {
@@ -237,7 +239,71 @@ const Home = () => {
       </section>
       <section
         className={`  ${Styles.vercelQuote}`}
-        style={{ margin: "auto", marginTop: "2rem" }}
+        style={{ margin: "auto", marginTop: "2rem", textAlign: "center" }}
+      >
+        <p>This Programming tool makes it easier for apps to work anywhere</p>
+        <div>
+          <VercelIcon />
+        </div>
+      </section>
+
+      <section className={`  ${Styles.deploySection}`}>
+        <Button startIcon={<DeployStart />} className={Styles.createBtn}>
+          Deploy
+        </Button>
+        <p className={Styles.inspHeader}>Above the clouds</p>
+        <p className={Styles.inspInfo}>
+          Get the scalability of serverless and the reusability of cloud. Deploy
+          to the edge, save your users time and yourself money.
+        </p>
+        <div className={Styles.deployProductContainer}>
+          <div className={Styles.deployProductCard}>
+            <div className={Styles.deployProductCardHeader}>
+              <p>Welcome to the world&rsquo;s most popular website builder</p>
+              <p>
+                42% of the web is built on WordPress. More bloggers, small
+                bussinesses, and Fortune 500 companies use WordPress than all
+                other options combined. Join the millions of people that call
+                WordPress.com home.
+              </p>
+              <button>Get Started</button>
+            </div>
+            <div className={Styles.deployProductCardFooter}>
+              <WordPressIcon />
+              <p>WordPress</p>
+            </div>
+          </div>
+          <div className={`${Styles.deployProductCard}`}>
+            <div className={Styles.deployProductCardHeader}>
+              <p>
+                Django makes it easier to build better web apps more quickly and
+                with less code.
+              </p>
+              <button>Get Started with Django</button>
+            </div>
+            <div className={Styles.deployProductCardFooter}>
+              <DjangoIcon />
+              <p>Django app</p>
+            </div>
+          </div>
+        </div>
+        <div className={Styles.deployFootnote}>
+          <p>No servers to maintain</p>
+          <p>Instant cold starts</p>
+          <p>Infinite scalability</p>
+        </div>
+        <div className={`  ${Styles.deployFooter}`}>
+          <div>
+            <p>Faster, cheaper & indefinitely scalable</p>
+          </div>
+          <div className={Styles.inspFooterCircleArrow}>
+            <CirclePointerIcon />{" "}
+          </div>
+        </div>
+      </section>
+      <section
+        className={`  ${Styles.vercelQuote}`}
+        style={{ margin: "auto", marginTop: "2rem", textAlign: "center" }}
       >
         <p>This Programming tool makes it easier for apps to work anywhere</p>
         <div>
